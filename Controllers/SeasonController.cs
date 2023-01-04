197d5ba3988bd280363a70e231bff30b85cab393
Scotterpop73 <sctzone@aol.com>
2023-01-04 09:37:31 -0800

    Created season controller

--- Controllers/SeasonController.cs
+++ Controllers/SeasonController.cs
@@ -11,9 +11,24 @@ namespace AttendanceTrackerBE.Controllers;
 [Route("[controller]")]
 public class SeasonController : ControllerBase
 {
+    //private readonly SeasonService _data
+    
+    //public SeasonController(SeasonService dataFromService)
+    //{
+        //_data = dataFromService;
+    //}
+
+
     [HttpPost]
     [Route("AddSeason")]
     public bool AddSeason(SeasonModel item){
         return true;
+        //return _data.AddSeason(item);
+    }
+
+    [HttpGet]
+    [Route("GetSeason")]
+    public SeasonModel GetSeason(SeasonModel item){
+        return item;
     }
 }
